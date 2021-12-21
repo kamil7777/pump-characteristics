c2894237a213bdeea3e81e0a8b05445c14bdb3a6
KamilSalakhov <antdead19@gmail.com>
2021-12-21 13:50:28 +0300

    refactor: delete MyChart

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,53 +29,9 @@
         document.getElementById('nap_text').innerHTML = napor2;
         document.getElementById('pow_text').innerHTML = power2;
 
-        //context.beginPath();
-        /*context.strokeStyle='red';
-        context.lineWidth='5';
-        context.lineTo((5*rashod2),( napor2));
-        context.stroke();*/
-
-        //context.beginPath();
         context.strokeStyle = 'blue';
         context.lineWidth = '5';
         context.lineTo(rashod2, (5 * power2));
         context.stroke();
     }
-
-    /* let ctx = document.getElementById('myChart');
-    let myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 10
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    } */
     });
